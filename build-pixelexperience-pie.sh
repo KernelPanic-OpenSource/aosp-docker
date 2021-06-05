8ad3a51f118e4b272eb0fc046f4f3764257df893
Trần Nguyễn Tuấn Anh <polarisdp@gmail.com>
2021-06-06 00:32:24 +0700

    Update build-pixelexperience-pie.sh

--- build-pixelexperience-pie.sh
+++ build-pixelexperience-pie.sh
@@ -1,5 +1,3 @@
-
-if [ "$1" = "docker" ]; then
     TEST_BRANCH=${TEST_BRANCH:-ten-plus}
     TEST_URL=${TEST_URL:-https://github.com/PixelExperience/manifest}
     repo init --depth 1 -u "$TEST_URL" -b "$TEST_BRANCH"
@@ -7,11 +5,3 @@ if [ "$1" = "docker" ]; then
     . build/envsetup.sh
     breakfast bullhead
     brunch bullhead
-else
-    args="bash run.sh docker" 
-    export AOSP_EXTRA_ARGS="-v $(cd $(dirname $0) && pwd -P)/$(basename $0):/usr/local/bin/run.sh:ro"
-    export AOSP_IMAGE="timleung/aosp:10.0-ten"
-   ./aosp $args
-
-    
-fi
